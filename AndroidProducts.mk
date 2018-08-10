be10efb5ecd26bd1bdd06e83d23c765228a97d0b
Bruno Martins <bgcngm@gmail.com>
2018-08-10 13:47:04 +0100

    pioneer: Consider lineage_pioneer product via the AOSP way
    Change-Id: Ibdeb62f0aa30933a09ac12ca825025e5f18574ab

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_pioneer.mk
+    $(LOCAL_DIR)/lineage_pioneer.mk
--- lineage_pioneer.mk
+++ lineage_pioneer.mk
@@ -14,11 +14,17 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/sony/pioneer/full_pioneer.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from pioneer device
+$(call inherit-product, device/sony/pioneer/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_pioneer
 PRODUCT_DEVICE := pioneer
 PRODUCT_BRAND := Sony
