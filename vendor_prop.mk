be749751783694cb46e3f13c53af83a87282ad40
LuK1337 <priv.luk@gmail.com>
2019-02-22 00:13:06 +0100

    pioneer: Update blobs from 50.2.A.0.400
    Change-Id: Id56443f728644a358d68919860fdb3e2390b5d3f

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -18,8 +18,8 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.semc.product.model=H3113 \
     ro.semc.version.fs=GENERIC \
-    ro.semc.version.fs_revision=50.1.A.13.123 \
+    ro.semc.version.fs_revision=50.2.A.0.342 \
     ro.semc.version.sw=1311-2845 \
-    ro.semc.version.sw_revision=50.1.A.13.123 \
+    ro.semc.version.sw_revision=50.2.A.0.342 \
     ro.semc.version.sw_type=user \
     ro.semc.version.sw_variant=GLOBAL-A1
