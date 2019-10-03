4754f8261af20729a169212829a44d10f018525a
Sujin Panicker <spanic@codeaurora.org>
2019-10-03 12:16:58 +0200

    pioneer: audio: Rename audio_platform_info to audio_platform_info_intcodec
    * fixes Audio with new CAF Audio Hals
    * refering to https://github.com/LineageOS/android_hardware_qcom_audio/commit/b904fbec4986fb371c81cf550f2615052874bf40
    
    Change-Id: I9adc4cffa8b3b9d0361d340ce062527425e0d4fa

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
 # NFC
