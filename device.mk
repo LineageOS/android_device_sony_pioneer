7eacbb37ddc122ac5ddc5ceb8bc4efdf7968cfe5
LuK1337 <priv.luk@gmail.com>
2020-02-16 00:33:19 +0100

    pioneer: overlay: Set config_deviceHardware{Wake,}Keys
    Change-Id: I9b0793107ecdb366594998d7dfd2a7a81d37502a

--- device.mk
+++ device.mk
@@ -24,6 +24,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
