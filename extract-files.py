f465b86f9db5cd6bef6b787a112a7e4c796a719b
LuK1337 <priv.luk@gmail.com>
2024-10-15 22:12:50 +0200

    pioneer: Switch to py extract utils
    Change-Id: I385df1ce3d67b291c7b6a543cb6581d2fc60b443

---
+++ extract-files.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/sony/nile-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/bin/qns', 'vendor/lib/libSonyIMX300PdafLibrary.so'): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    ('vendor/lib/vendor.somc.hardware.security.secd@1.0.so', 'vendor/lib64/vendor.somc.hardware.security.secd@1.0.so'): blob_fixup()
+        .add_needed('libhidlbase_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'pioneer',
+    'sony',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'nile-common', module.vendor)
+    utils.run()
