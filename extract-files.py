f1451b5a3882aa324ec9298826ef9e02a65afa00
LuK1337 <priv.luk@gmail.com>
2025-01-07 10:47:00 +0100

    pioneer: Clear LIBC_PRIVATE symbol versions for libmmcamera_faceproc
    Change-Id: I1565a6fef5070377650e62dc3e3a45bfd2bfaf5b

--- extract-files.py
+++ extract-files.py
@@ -24,6 +24,10 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     'vendor/lib/libSonyIMX300PdafLibrary.so': blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
+        .clear_symbol_version('__aeabi_memcpy')
+        .clear_symbol_version('__aeabi_memset')
+        .clear_symbol_version('__gnu_Unwind_Find_exidx'),
     ('vendor/lib/vendor.somc.hardware.security.secd@1.0.so', 'vendor/lib64/vendor.somc.hardware.security.secd@1.0.so'): blob_fixup()
         .add_needed('libhidlbase_shim.so'),
 }  # fmt: skip
