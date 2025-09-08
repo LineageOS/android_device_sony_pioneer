465ddee374492a0cb852a0a56c88867016b0d688
LuK1337 <priv.luk@gmail.com>
2025-09-08 15:06:30 +0200

    pioneer: hardware/qcom-caf/msm8998 -> hardware/qcom-caf/sdm660
    Change-Id: Ie27f7af089af176bda06df9db34fce6dccbadbbd

--- extract-files.py
+++ extract-files.py
@@ -14,7 +14,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'hardware/qcom-caf/msm8998',
+    'hardware/qcom-caf/sdm660',
     'hardware/qcom-caf/wlan',
     'vendor/sony/nile-common',
 ]
